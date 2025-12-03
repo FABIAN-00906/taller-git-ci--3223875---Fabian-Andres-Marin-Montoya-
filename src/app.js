7648ca2419a2f3da79c84300e34ecafe65fefc47
Fabian Andres Marin Montoya <fabianandres.marin01@gmail.com>
2025-12-02 20:25:57 -0500

    feat(ui): implementar función agregarItem para añadir elementos a la lista

--- src/app.js
+++ src/app.js
@@ -4,6 +4,16 @@ function suma(a, b) {
 
 console.log("App lista");
 
+if (typeof document !== 'undefined') {
+  function agregarItem(texto) {
+    const lista = document.getElementById('lista');
+    if (!lista) return;
+
+    const li = document.createElement('li');
+    li.textContent = texto;
+    lista.appendChild(li);
+  }}
+
 if (typeof module !== 'undefined') {
   module.exports = suma;
 }
